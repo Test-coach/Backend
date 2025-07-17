b6221330ea8fdd66806983c994afaeebce28ba25
Arpit Puri <arpit.puri6265@gmail.com>
2025-07-18 00:06:52 +0530

    Add new endpoints for course and coupon management in OpenAPI and backend
    - Introduced endpoints for creating course-specific and global coupons, fetching all coupons, and managing individual coupons by code.
    - Updated OpenAPI documentation to reflect the new coupon management functionalities.
    - Enhanced admin route definitions for better clarity and organization.
    - Implemented necessary request and response schemas for the new endpoints, ensuring proper validation and error handling.

--- src/modules/admin/routes/exam.route.js
+++ src/modules/admin/routes/exam.route.js
@@ -4,13 +4,13 @@ const { isAdmin } = require('../middleware/admin.auth.middleware');
 
 const router = express.Router();
 
-// POST /admin/
+// POST
 router.post('/course', isAdmin, examController.createCourse);
 router.post('/tests', isAdmin, examController.createTest);
 router.post('/add-course-coupon', isAdmin, examController.createCourseCoupon);
 router.post('/add-coupon', isAdmin, examController.createCoupon);
 
-//Get /admin/
+//Get
 router.get('/courses', isAdmin, examController.getAllCourses);
 router.get('/exams', isAdmin, examController.getAllExams);
 router.get('/course/:courseName/tests', isAdmin, examController.getTestsByCourseName);
